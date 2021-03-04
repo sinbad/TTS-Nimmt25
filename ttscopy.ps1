7055a08db786b032209cbba754f27ab2b656f26d
Steve Streeting <steve@stevestreeting.com>
2021-03-04 11:19:41 +0000

    Add copy script from TTS temp folder to here

---
+++ ttscopy.ps1
@@ -0,0 +1,3 @@
+# Copies whatever is the active TTS session to this folder
+# When editing in TTS all the scripts are in this temp folder
+copy '~\AppData\Local\Temp\TabletopSimulator\Tabletop Simulator Lua\*.lua' .
